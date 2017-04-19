4bf046e5da9b38dd132a9a0d87c2799ac539aae7
Adam J <adamjac@kth.se>
2017-04-19 18:23:07 +0200

    only component available

--- client/ui/body.js
+++
@@ -1,9 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-import './body.html';
-
-Template.layout.helpers({
-  rendered: function(){
-    
-  }
-});
--- private/phantomDriver.js
+++
@@ -1,7 +0,0 @@
-var page = require('webpage').create();
-
-page.open('http://google.com', function() {
-    console.log('Page Loaded');
-    page.render('google.png');
-    phantom.exit();
-});
